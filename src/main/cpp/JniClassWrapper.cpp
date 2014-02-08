56448338263925bd77935b4f0188abe8265ec7a3
Nik Reiman <nik.reiman@gmail.com>
2014-02-08 18:30:36 +0100

    Rename file, add method stubs

---
+++ src/main/cpp/JniClassWrapper.cpp
@@ -0,0 +1,39 @@
+#include "JniClassWrapper.h"
+
+JniClassWrapper::JniClassWrapper(JNIEnv *env) {
+
+}
+
+JniClassWrapper::~JniClassWrapper() {
+
+}
+
+jmethodID JniClassWrapper::getMethod(const char *field_name) {
+  return NULL;
+}
+
+jmethodID JniClassWrapper::getField(const char* field_name) {
+  return NULL;
+}
+
+template<typename TypeName>
+TypeName JniClassWrapper::getFieldValue(jobject instance, const char* field_name) {
+  return NULL;
+}
+
+void JniClassWrapper::cacheMethod(const char* method_name, const char* return_type, ...) {
+
+}
+
+void JniClassWrapper::cacheField(const char* field_name) {
+
+}
+
+template<typename FunctionPtr>
+JNINativeMethod JniClassWrapper::makeNativeMethod(const char *method_name, FunctionPtr *function, const char *return_type, ...) {
+  return NULL;
+}
+
+bool JniClassWrapper::registerNativeMethods(JNIEnv *env, const std::string &class_name, const std::vector<JNINativeMethod> &methods) {
+  return false;
+}
---
+++ src/main/cpp/JniClassWrapper.h
@@ -0,0 +1,53 @@
+#ifndef __JniClassWrapper_h__
+#define __JniClassWrapper_h__
+
+#include <jni.h>
+#include <map>
+#include <string>
+#include <vector>
+
+class JniClassWrapper {
+public:
+  /**
+  * Create a new JNI class wrapper.
+  * @param env JVM environment
+  */
+  explicit __declspec(dllexport) JniClassWrapper(JNIEnv *env);
+
+private:
+  // Disallow no-arg constructor
+  JniClassWrapper() {}
+  // Disallow copy constructor
+  JniClassWrapper(const JniClassWrapper&) {}
+
+public:
+  __declspec(dllexport)
+  virtual ~JniClassWrapper();
+
+  virtual void initialize(JNIEnv *env) = 0;
+  virtual const char* getClassName() const = 0;
+
+  virtual JniClassWrapper* fromJavaObject(JNIEnv *env, jobject javaObject) const = 0;
+  virtual jobject toJavaObject(JniClassWrapper *nativeObject) = 0;
+
+public:
+  jmethodID __declspec(dllexport) getMethod(const char *field_name);
+  jmethodID __declspec(dllexport) getField(const char* field_name);
+  template<typename TypeName> __declspec(dllexport)
+  TypeName getFieldValue(jobject instance, const char* field_name);
+
+protected:
+  void __declspec(dllexport) cacheMethod(const char* method_name, const char* return_type, ...);
+  void __declspec(dllexport) cacheField(const char* field_name);
+
+  template<typename FunctionPtr> __declspec(dllexport)
+  JNINativeMethod makeNativeMethod(const char *method_name, FunctionPtr *function, const char *return_type, ...);
+  bool __declspec(dllexport)
+  registerNativeMethods(JNIEnv *env, const std::string &class_name, const std::vector<JNINativeMethod> &methods);
+
+protected:
+  static std::map<std::string, jmethodID> _methods;
+  static std::map<std::string, jfieldID> _fields;
+};
+
+#endif // __JniClassWrapper_h__
