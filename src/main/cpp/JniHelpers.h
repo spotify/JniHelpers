f193b84cc1656bcfc3594a47759853f9f80186b3
Nik Reiman <nik.reiman@gmail.com>
2014-02-09 10:15:57 +0100

    Add prefix header

---
+++ src/main/cpp/JniHelpers.h
@@ -0,0 +1,14 @@
+#ifndef __JniHelpers_h__
+#define __JniHelpers_h__
+
+#if WIN32
+#define EXPORT __declspec(dllexport)
+#endif
+
+#include "JniClassUtils.h"
+#include "JniClassWrapper.h"
+#include "JniGlobalRef.h"
+#include "JniLocalRef.h"
+#include "JniThreadUtils.h"
+
+#endif // __JniHelpers_h__
