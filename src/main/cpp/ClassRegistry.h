d391566956d5e78fa39f5256273602dfae632a21
Nik Reiman <nik.reiman@gmail.com>
2014-02-16 17:02:39 +0100

    Extra error checking, exceptions thrown

--- src/main/cpp/ClassRegistry.h
+++ src/main/cpp/ClassRegistry.h
@@ -21,24 +21,30 @@ public:
   ClassRegistry() {}
   virtual ~ClassRegistry() {}
 
-  virtual void add(const ClassWrapper *item) {
+  virtual void add(JNIEnv *env, const ClassWrapper *item) {
     if (item == NULL) {
-      JNIEnv *env = JavaThreadUtils::getEnvForCurrentThread();
-      if (env != NULL) {
-        JavaExceptionUtils::throwRuntimeException(env, "Can't add null item to map");
-      }
+      JavaExceptionUtils::throwExceptionOfType(env, kTypeIllegalArgumentException,
+        "Can't add null item to registry");
       return;
     } else if (item->getCanonicalName() == NULL || strlen(item->getCanonicalName()) == 0) {
-      JNIEnv *env = JavaThreadUtils::getEnvForCurrentThread();
-      if (env != NULL) {
-        JavaExceptionUtils::throwRuntimeException(env, "Can't item with empty canonical name to map");
-      }
+      JavaExceptionUtils::throwExceptionOfType(env, kTypeIllegalArgumentException,
+        "Can't add item with empty canonical name to registry");
+    } else if (item->getClass().get() == NULL) {
+      JavaExceptionUtils::throwExceptionOfType(env, kTypeIllegalArgumentException,
+        "Can't add uninitialized ClassWrapper to registry");
     } else {
       _classes[item->getCanonicalName()] = item;
     }
   }
 
   virtual const ClassWrapper* get(const char* name) {
+    if (name == NULL) {
+      JNIEnv *env = JavaThreadUtils::getEnvForCurrentThread();
+      JavaExceptionUtils::throwExceptionOfType(env, kTypeIllegalArgumentException,
+        "Can't call get() with NULL");
+      return NULL;
+    }
+
     std::map<std::string, const ClassWrapper*>::iterator iter = _classes.find(name);
     return iter != _classes.end() ? _classes[name] : NULL;
   }
@@ -49,12 +55,14 @@ public:
     TypeName *result = new TypeName();
     const char *name = result->getCanonicalName();
     if (name == NULL || strlen(name) == 0) {
-      JavaExceptionUtils::throwRuntimeException(env, "Could not find canonical name for class");
+      JavaExceptionUtils::throwExceptionOfType(env, kTypeIllegalArgumentException,
+        "Could not find canonical name for class");
       return NULL;
     }
     const TypeName *classInfo = dynamic_cast<const TypeName*>(get(name));
     if (classInfo == NULL) {
-      JavaExceptionUtils::throwRuntimeException(env, "No class information registered for '%s'", name);
+      JavaExceptionUtils::throwExceptionOfType(env, kTypeIllegalStateException,
+        "No class information registered for '%s'", name);
       return NULL;
     }
     result->merge(classInfo);
