86c3d354fa3e0aaad8679a5c2f58b1b42666a08e
Nik Reiman <nik.reiman@gmail.com>
2014-02-17 16:47:39 +0100

    Move types to separate header

--- src/main/cpp/JniHelpersCommon.h
+++ src/main/cpp/JniHelpersCommon.h
@@ -1,6 +1,9 @@
 #ifndef __JniHelpersCommon_h__
 #define __JniHelpersCommon_h__
 
+#include "JniTypes.h"
+#include <jni.h>
+
 // Disable some annoying compiler warnings
 #if WIN32
 #pragma warning(disable: 4996) // "Security" warnings for vsnprintf
@@ -11,29 +14,5 @@
 #else
 #define EXPORT
 #endif
-
-#include <jni.h>
-
-#define kTypeInt "I"
-#define kTypeLong "J"
-#define kTypeFloat "F"
-#define kTypeDouble "D"
-#define kTypeBool "Z"
-#define kTypeByte "B"
-#define kTypeVoid "V"
-#define kTypeString "Ljava/lang/String;"
-#define kTypeClass(x) "L" #x ";"
-#define kTypeJavaClass(x) "Ljava/lang/" #x ";"
-
-// Exceptions
-#define kTypeException kTypeJavaClass(Exception)
-#define kTypeRuntimeException kTypeJavaClass(RuntimeException)
-#define kTypeIllegalArgumentException kTypeJavaClass(IllegalArgumentException)
-#define kTypeIllegalStateException kTypeJavaClass(IllegalStateException)
-
-// Array builder macro
-#define kTypeArray(x) "[" x
-
-#define TYPE_EQUALS(_TYPE1, _TYPE2) (strcmp(_TYPE1, _TYPE2) == 0)
 
 #endif // __JniHelpersCommon_h__
---
+++ src/main/cpp/JniTypes.h
@@ -0,0 +1,28 @@
+#ifndef __JniTypes_h__
+#define __JniTypes_h__
+
+#include <string.h>
+
+#define kTypeInt "I"
+#define kTypeLong "J"
+#define kTypeFloat "F"
+#define kTypeDouble "D"
+#define kTypeBool "Z"
+#define kTypeByte "B"
+#define kTypeVoid "V"
+#define kTypeString "Ljava/lang/String;"
+#define kTypeClass(x) "L" #x ";"
+#define kTypeJavaClass(x) "Ljava/lang/" #x ";"
+
+// Exceptions
+#define kTypeException kTypeJavaClass(Exception)
+#define kTypeRuntimeException kTypeJavaClass(RuntimeException)
+#define kTypeIllegalArgumentException kTypeJavaClass(IllegalArgumentException)
+#define kTypeIllegalStateException kTypeJavaClass(IllegalStateException)
+
+// Array builder macro
+#define kTypeArray(x) "[" x
+
+#define TYPE_EQUALS(_TYPE1, _TYPE2) (strcmp(_TYPE1, _TYPE2) == 0)
+
+#endif // __JniTypes_h__
