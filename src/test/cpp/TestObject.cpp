af87ba00b01924d26a5f5c9b21995d20f4791b1f
Nik Reiman <nik.reiman@gmail.com>
2014-02-20 10:44:27 +0100

    TestObject merges in constructor
    Normally this isn't the desired behavior, however the way that we
    initialize objects in tests is also a bit unusual so it makes sense.
    Explanatory comment added.

--- src/test/cpp/TestObject.cpp
+++ src/test/cpp/TestObject.cpp
@@ -7,6 +7,11 @@ s(), i(0), f(0.0f) {
 TestObject::TestObject(JNIEnv *env) : ClassWrapper(env),
 s(), i(0), f(0.0f) {
   initialize(env);
+  // Set up field mappings for the instance. Normally this isn't necessary,
+  // since the global instance is typically used for merging into other objects.
+  // If you want to actually use the initialized instance in combination with
+  // setJavaObject(), you should call this though.
+  merge(this);
 }
 
 void TestObject::initialize(JNIEnv *env) {
