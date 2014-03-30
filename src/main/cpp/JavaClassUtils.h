bd2ee465de2e7fa6300eb6aa0bd944694c72da10
Nik Reiman <nik.reiman@gmail.com>
2014-03-30 15:57:59 +0200

    Document JavaClassUtils

--- src/main/cpp/JavaClassUtils.h
+++ src/main/cpp/JavaClassUtils.h
@@ -8,6 +8,19 @@
 namespace spotify {
 namespace jni {
 
+/**
+ * @brief Convenience macro to create canonical name for a class
+ *
+ * Note that the class name should *not* be a quoted string, otherwise the quotes
+ * will be added literally to the string. Example usage:
+ *
+ * #define PACKAGE "com/example/stuff"
+ * MAKE_CANONICAL_NAME(PACKAGE, Foo)
+ *
+ * In the above example, the PACKAGE definition should ideally be placed in a header
+ * file which is accessible to your package sources. The above macro will then produce
+ * the string "com/example/stuff/Foo".
+ */
 #define MAKE_CANONICAL_NAME(_PACKAGE, _CLASS) _PACKAGE "/" #_CLASS
 
 class JavaClassUtils {
@@ -18,11 +31,49 @@ private:
   virtual ~JavaClassUtils() {}
 
 public:
+  /**
+   * @brief Cache an instance of the Java ClassLoader
+   * @param env JNIEnv
+   *
+   * If you intend to use findClass() with a Java ClassLoader, then this method will be
+   * invoked automatically to initialize a static ClassWrapper instance of this class,
+   * which is needed by findClass() to do the actual lookup. Since initialization of
+   * ClassWrapper instances may take a bit of time, this method is exposed in case you
+   * wish to manually call it during JNI initialization.
+   *
+   * In most cases, it should not be necessary to explicitly call this method from your
+   * code.
+   */
   static EXPORT void setJavaClassLoader(JNIEnv *env);
-  // TODO: Not sure if this is the best interface here...
-  static EXPORT jclass findClass(JNIEnv *env, const char *class_name, bool useClassLoader);
 
-  static EXPORT void makeNameForSignature(std::string &receiver, const char *class_name);
+  /**
+   * @brief Look up a Java class object
+   * @param env JNIEnv
+   * @param class_name Canonical class name
+   * @param use_class_loader Use a Java ClassLoader instance. This is not necessary for
+   *                         looking up Java classes, however it may be needed in other
+   *                         cases, particularly when trying to load your own classes in
+   *                         Android.
+   * @return Java class object, or NULL if none was found
+   *
+   * This method will throw NoClassDefFoundError if the class could not be found.
+   */
+  static EXPORT jclass findClass(JNIEnv *env, const char *class_name, bool use_class_loader);
+
+  /**
+   * @brief Make a name safe to pass to a method requiring JNI signatures
+   * @param receiver String to recieve the generated signature
+   * @param name Name to convert
+   *
+   * When looking up field or method names which involve another class name, it may be
+   * necessary to convert these to the corresponding JNI signature types (for example,
+   * Lcom/example/MyClass; for com.example.MyClass). This method will create a JNI
+   * signature string based on a canonical name.
+   *
+   * Passing a JNI signature to this class should have no effect, it will simply return
+   * the same string to the receiver.
+   */
+  static EXPORT void makeNameForSignature(std::string &receiver, const char *name);
 
   /**
     * @brief Make a method signature from a variable list of arguments
@@ -33,6 +84,13 @@ public:
     *            last argument to this method, unexpected behavior will occur!
     */
   static EXPORT void makeSignature(std::string &receiver, const char *return_type, ...);
+
+  /**
+   * @brief Make a method signature from a variable list of arguments
+   * @param receiver String to receive the generated signature
+   * @param return_type Return type of function (see JniTypes.h)
+   * @param arguments List of arguments which function takes
+   */
   static EXPORT void makeSignatureWithList(std::string &receiver, const char *return_type, va_list arguments);
 };
 
