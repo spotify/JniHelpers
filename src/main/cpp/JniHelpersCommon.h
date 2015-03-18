a486a0d1d3578ca559c0fc1c1a4ef6df2b2265a2
Mattias Jansson <mjansson@spotify.com>
2015-03-18 13:16:05 +0100

    enable separate warn/error and debug/info logging

--- src/main/cpp/JniHelpersCommon.h
+++ src/main/cpp/JniHelpersCommon.h
@@ -73,18 +73,32 @@
 #define ENABLE_LOGGING 0
 #endif
 
+// Determines whether debug/info logging messages will be printed by JniHelpers
+#ifndef ENABLE_LOGGING_DEBUG
+#define ENABLE_LOGGING_DEBUG 0
+#endif
+
 #if ENABLE_LOGGING
 #if ANDROID
 #include <android/log.h>
 #define LOGGING_TAG "JniHelpers"
-
+#if ENABLE_LOGGING_DEBUG
 #define LOG_DEBUG(...) __android_log_print(ANDROID_LOG_INFO, LOGGING_TAG, __VA_ARGS__)
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, LOGGING_TAG, __VA_ARGS__)
+#else
+#define LOG_DEBUG(...)
+#define LOG_INFO(...)
+#endif
 #define LOG_WARN(...) __android_log_print(ANDROID_LOG_WARN, LOGGING_TAG, __VA_ARGS__)
 #define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOGGING_TAG, __VA_ARGS__)
 #else
+#if ENABLE_LOGGING_DEBUG
 #define LOG_DEBUG(...) fprintf(stderr, "DEBUG: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #define LOG_INFO(...) fprintf(stderr, "INFO: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
+#else
+#define LOG_DEBUG(...)
+#define LOG_INFO(...)
+#endif
 #define LOG_WARN(...) fprintf(stderr, "WARN: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #define LOG_ERROR(...) fprintf(stderr, "ERROR: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #endif
